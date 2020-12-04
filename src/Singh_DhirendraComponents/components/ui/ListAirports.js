d6025a5559fbc24854ae70581141c3e610047902
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2020-12-05 04:59:43 +0530

    UI enhanced

--- src/Singh_DhirendraComponents/components/ui/ListAirports.js
+++ src/Singh_DhirendraComponents/components/ui/ListAirports.js
@@ -208,7 +208,7 @@ class ListAirports extends Component {
                                 d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                         </svg>{airport.airportLocation}</td>
                       <td><section><div className="container row"><div className="col-md-3"><button className="btn btn-outline-success btn-lg" onClick={()=>{this.updateAirportClicked(airport.airportId)}}>
-                        <svg width="1em" height="1em" viewBox="0 0 16 18" className="bi bi-pencil-fill"
+                        <svg width="1.5em" height="1.5em" viewBox="0 0 16 18" className="bi bi-pencil-fill"
                              fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                           <path fill-rule="evenodd"
                                 d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
@@ -221,11 +221,10 @@ class ListAirports extends Component {
                       {/*  </Button>*/}
                       {/*</td>*/}
                       <td><section><div className="container row"><div className="col-md-3"><button className="btn btn-outline-danger btn-lg" onClick={()=>{this.deleteAirportClicked(airport.airportId)}}>
-                        <svg width="1.0em" height="1em" viewBox="0 0 16 18"
-                             className="bi bi-exclamation-triangle-fill" fill="currentColor"
+                        <svg width="1.5em" height="1.5em" viewBox="0 0 16 18" className="bi bi-x" fill="currentColor"
                              xmlns="http://www.w3.org/2000/svg">
                           <path fill-rule="evenodd"
-                                d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 5zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                         </svg></button></div></div></section></td>
                     </tr>
                   )}
